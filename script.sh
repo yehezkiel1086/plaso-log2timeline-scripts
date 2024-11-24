6ba50038f551cefe08c5cf15c5740dc6cf56b422
yehezkiel1086 <yehezkiel1086@gmail.com>
2024-11-24 03:15:06 -0500

    feat: add create plaso storage and timeline without parsing or filtering

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Create plaso storage without filter or parsing
-log2timeline.py --storage-file unfiltered-timeline.plaso Webserver.E01
+#log2timeline.py --storage-file \
+#/home/ubuntu/Documents/plaso-storage/unfiltered-timeline.plaso \
+#/home/ubuntu/Documents/Webserver.E01
 
-# Create timeline
-#psort.py -o dynamic -w registrar.csv timeline.plaso
+# Create timeline for unfiltered-timeline
+psort.py -o dynamic -w \
+/home/ubuntu/Documents/reports/unfiltered-timeline.csv \
+/home/ubuntu/Documents/plaso-storage/unfiltered-timeline.plaso
