723a3d309bc199fcd0cf6ee1a32a3f57a97a6285
yehezkiel1086 <yehezkiel1086@gmail.com>
2024-11-24 03:47:30 -0500

    feat: add webhist parsed dump and report

--- script.sh
+++ script.sh
@@ -6,6 +6,16 @@
 #/home/ubuntu/Documents/Webserver.E01
 
 # Create timeline for unfiltered-timeline
+#psort.py -o dynamic -w \
+#/home/ubuntu/Documents/reports/unfiltered-timeline.csv \
+#/home/ubuntu/Documents/plaso-storage/unfiltered-timeline.plaso
+
+# Create plaso storage without filter parsed as webhist
+#log2timeline.py --parsers webhist --storage-file \
+#/home/ubuntu/Documents/plaso-storage/unfiltered-webhist.dump \
+#/home/ubuntu/Documents/Webserver.E01
+
+# Create timeline report for unfiltered-webhist
 psort.py -o dynamic -w \
-/home/ubuntu/Documents/reports/unfiltered-timeline.csv \
-/home/ubuntu/Documents/plaso-storage/unfiltered-timeline.plaso
+/home/ubuntu/Documents/reports/unfiltered-webhist.csv \
+/home/ubuntu/Documents/plaso-storage/unfiltered-webhist.dump
